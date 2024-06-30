3593f0b37db90feb9da6cb00bdb05225dd1def56
Bhautikvekariya <127239928+Bhautikvekariya@users.noreply.github.com>
2024-06-30 15:28:58 +0530

    npm data

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+// let math= require("./math");
+// console.log(math.pi);
+
+// let info= require("./Fruits");
+// console.log(info[0]);
+
+import {sum,pi,mul} from "./math.js";
+import {generate} from "random-words";
+
+console.log(generate());
+console.log(sum(1,2));
